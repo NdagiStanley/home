ef9df5d3d9c41a625ac33503c1e077ac22b7ae1d
Ndagi Stanley <ndagis@gmail.com>
2021-06-05 09:36:54 +0300

    Update Mantra component

--- src/.vuepress/components/Mantra.vue
+++ src/.vuepress/components/Mantra.vue
@@ -3,6 +3,6 @@
     <div>
       My mantra: <i>invoke the builder within and serve</i>
     </div>
-    <a href="/about#">What does this mean?</a>
+    <a href="/about#what-do-i-do">What does this mean?</a>
   </div>
 </template>
